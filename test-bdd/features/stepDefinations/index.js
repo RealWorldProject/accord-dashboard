f50aa3a3ed1d24396d4c3d3e8d33386cad3f6fd0
gaurabrijal073 <gaurabrijal073@gmail.com>
2021-07-28 20:35:52 +0545

    Book add test pass

--- test-bdd/features/stepDefinations/index.js
+++ test-bdd/features/stepDefinations/index.js
@@ -1,6 +1,6 @@
 const { expect } = require("chai");
 const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
-const { Builder, By, Key, until, sleep } = require("selenium-webdriver");
+const { Builder, By, Key, until, sleep, Browser } = require("selenium-webdriver");
 const { delay } = require("../utils/delay");
 
 // Given("Test login functionality", { timeout: 30000 }, async function () {
@@ -15,20 +15,25 @@ const { delay } = require("../utils/delay");
 //   // await driver.quit();
 // });
 
-Given("Test Add Category functionality", { timeout: 30000 }, async function () {
+Given("Test Add Category functionality", { timeout: 50000 }, async function () {
   let driver = await new Builder().forBrowser("chrome").build();
   await driver.get("http://localhost:3000/categories");
   await driver.findElement(By.id("username")).sendKeys("superuser@accord.com");
   await driver.findElement(By.id("password")).sendKeys("superuser123");
   await driver.findElement(By.id("loginBtn")).click();
   await driver.sleep(delay);
+  await driver.navigate().refresh();
   await driver.findElement(By.xpath('//*[@id="root"]/div/div[1]/div/div/div[2]/ul[1]/a[3]')).click();
+  await driver.sleep(delay);
   await driver.findElement(By.xpath('//*[@id="root"]/div/div[1]/main/div[2]/button')).click();
-  await driver.findElement(By.id("category")).sendKeys("action");
+  await driver.findElement(By.id("category")).sendKeys("mystery");
   await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/div[3]')).click();
+  await driver.sleep(delay);
+  await driver.sleep(delay);
   await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[3]/button[1]')).click();
+  await driver.sleep(delay);
   
-  await driver.wait(until.elementLocated(By.xpath('//*[@id="MUIDataTableBodyRow-0"]/td[5]/div[2]/div/button[1]')), 300);
-  expect(await driver.wait(until.elementLocated(By.xpath('//*[@id="MUIDataTableBodyRow-0"]/td[2]/div[2]'))));
+  await driver.wait(until.elementLocated(By.xpath('//*[@id="MUIDataTableBodyRow-7"]/td[2]/div[2]')), 300);
+  expect(await driver.wait(until.elementLocated(By.xpath('//*[@id="MUIDataTableBodyRow-7"]/td[2]/div[2]'))));
   // await driver.quit();
 });
