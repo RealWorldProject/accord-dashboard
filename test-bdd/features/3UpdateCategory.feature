f7e19c86c382b713ea6cc714910cd286fd1fa69b
gaurabrijal073 <gaurabrijal073@gmail.com>
2021-07-29 21:15:35 +0545

    Added update category test code

---
+++ test-bdd/features/3UpdateCategory.feature
@@ -0,0 +1,5 @@
+Feature: To Update Category 
+  As an admin, I need to Update categories of the system.
+
+  Scenario: Admin provides updated category name and press edit Category button to update category.
+    Given Test Update Category functionality
--- test-bdd/features/stepDefinations/index.js
+++ test-bdd/features/stepDefinations/index.js
@@ -2,6 +2,7 @@ const { expect } = require("chai");
 const { Given, When, Then, Before, After } = require("@cucumber/cucumber");
 const { Builder, By, Key, until, sleep, Browser } = require("selenium-webdriver");
 const { delay } = require("../utils/delay");
+const { keys } = require("@material-ui/core/styles/createBreakpoints");
 
 // Given("Test login functionality", { timeout: 30000 }, async function () {
 //   let driver = await new Builder().forBrowser("chrome").build();
@@ -15,7 +16,30 @@ const { delay } = require("../utils/delay");
 //   // await driver.quit();
 // });
 
-Given("Test Add Category functionality", { timeout: 50000 }, async function () {
+// Given("Test Add Category functionality", { timeout: 50000 }, async function () {
+//   let driver = await new Builder().forBrowser("chrome").build();
+//   await driver.get("http://localhost:3000/categories");
+//   await driver.findElement(By.id("username")).sendKeys("superuser@accord.com");
+//   await driver.findElement(By.id("password")).sendKeys("superuser123");
+//   await driver.findElement(By.id("loginBtn")).click();
+//   await driver.sleep(delay);
+//   await driver.navigate().refresh();
+//   await driver.findElement(By.xpath('//*[@id="root"]/div/div[1]/div/div/div[2]/ul[1]/a[3]')).click();
+//   await driver.sleep(delay);
+//   await driver.findElement(By.xpath('//*[@id="root"]/div/div[1]/main/div[2]/button')).click();
+//   await driver.findElement(By.id("category")).sendKeys("mystery");
+//   await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/div[3]')).click();
+//   await driver.sleep(delay);
+//   await driver.sleep(delay);
+//   await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[3]/button[1]')).click();
+//   await driver.sleep(delay);
+  
+//   await driver.wait(until.elementLocated(By.xpath('//*[@id="MUIDataTableBodyRow-7"]/td[2]/div[2]')), 300);
+//   expect(await driver.wait(until.elementLocated(By.xpath('//*[@id="MUIDataTableBodyRow-7"]/td[2]/div[2]'))));
+//   // await driver.quit();
+// });
+
+Given("Test Update Category functionality", { timeout: 50000 }, async function () {
   let driver = await new Builder().forBrowser("chrome").build();
   await driver.get("http://localhost:3000/categories");
   await driver.findElement(By.id("username")).sendKeys("superuser@accord.com");
@@ -25,15 +49,13 @@ Given("Test Add Category functionality", { timeout: 50000 }, async function () {
   await driver.navigate().refresh();
   await driver.findElement(By.xpath('//*[@id="root"]/div/div[1]/div/div/div[2]/ul[1]/a[3]')).click();
   await driver.sleep(delay);
-  await driver.findElement(By.xpath('//*[@id="root"]/div/div[1]/main/div[2]/button')).click();
-  await driver.findElement(By.id("category")).sendKeys("mystery");
-  await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[2]/div[3]')).click();
+  await driver.findElement(By.xpath('//*[@id="MUIDataTableBodyRow-0"]/td[4]/div[2]/div/button[1]')).click();
+  await driver.findElement(By.id("category")).sendKeys(" Updated");
+  await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[3]/button[1]/span[1]')).click();
   await driver.sleep(delay);
-  await driver.sleep(delay);
-  await driver.findElement(By.xpath('/html/body/div[2]/div[3]/div/div[3]/button[1]')).click();
-  await driver.sleep(delay);
+  await driver.navigate().refresh();
   
-  await driver.wait(until.elementLocated(By.xpath('//*[@id="MUIDataTableBodyRow-7"]/td[2]/div[2]')), 300);
-  expect(await driver.wait(until.elementLocated(By.xpath('//*[@id="MUIDataTableBodyRow-7"]/td[2]/div[2]'))));
+  await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/div[2]')), 300);
+  expect(await driver.wait(until.elementLocated(By.xpath('//*[@id="root"]/div/div[2]'))));
   // await driver.quit();
 });
