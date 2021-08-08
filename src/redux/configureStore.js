79889c3578d741f7d2a8b2973414be2eaa02ab9c
asharyapandey <asharyap7@gmail.com>
2021-08-08 20:37:14 +0545

    Feature: added orders section

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,18 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import bookSlice from "./slices/book.slice";
 import categorySlice from "./slices/category.slice";
+import orderSlice from "./slices/order.slice";
 import snackbarSlice from "./slices/snackbar.slice";
 import userSlice from "./slices/user.slice";
+import usersSlice from "./slices/users.slice";
 
 const reducer = combineReducers({
 	user: userSlice,
 	category: categorySlice,
 	snackbar: snackbarSlice,
 	book: bookSlice,
+	users: usersSlice,
+	order: orderSlice,
 });
 
 const store = configureStore({
