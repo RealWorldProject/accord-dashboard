78eafe58596327f7cabacc844f633ef307c905a7
asharyapandey <asharyap7@gmail.com>
2021-08-25 21:11:51 +0545

    fix/some color fixes

--- src/components/Data/BookPieChart.js
+++ src/components/Data/BookPieChart.js
@@ -3,20 +3,20 @@ import { Pie } from "react-chartjs-2";
 
 const BookPieChart = ({ bookData }) => {
 	const data = {
-		labels: ["Approved", "Rejected", "Pending"],
+		labels: ["Pending", "Approved", "Rejected"],
 		datasets: [
 			{
 				label: "# of Books",
 				data: bookData,
 				backgroundColor: [
-					"rgba(255, 99, 132, 0.2)",
-					"rgba(54, 162, 235, 0.2)",
 					"rgba(255, 206, 86, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 99, 132, 0.2)",
 				],
 				borderColor: [
-					"rgba(255, 99, 132, 1)",
-					"rgba(54, 162, 235, 1)",
-					"rgba(255, 206, 86, 1)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 99, 132, 0.2)",
 				],
 				borderWidth: 1,
 			},
